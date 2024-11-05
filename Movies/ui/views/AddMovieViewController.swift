aa23af79d6f3d4f157e2a591bce50f13a6ed7610
omercsx <omercagris@gmail.com>
2024-11-05 10:57:29 -0500

    update and add

--- Movies/ui/views/AddMovieViewController.swift
+++ Movies/ui/views/AddMovieViewController.swift
@@ -8,7 +8,21 @@
 import UIKit
 
 class AddMovieViewController: UIViewController {
+    @IBOutlet var titleTF: UITextField!
+    @IBOutlet var ratingTF: UITextField!
+    @IBOutlet var studioTF: UITextField!
+
     override func viewDidLoad() {
         super.viewDidLoad()
+    }
+
+    @IBAction func buttonSave(_ sender: UIButton) {
+        if let title = titleTF.text, let rating = ratingTF.text, let studio = studioTF.text {
+            save(title: title, rating: rating, studio: studio)
+        }
+    }
+
+    func save(title: String, rating: String, studio: String) {
+        print("Saved: \(title), \(rating), \(studio)")
     }
 }
--- Movies/ui/views/MovieDetailViewController.swift
+++ Movies/ui/views/MovieDetailViewController.swift
@@ -8,7 +8,23 @@
 import UIKit
 
 class MovieDetailViewController: UIViewController {
+    @IBOutlet var titleTF: UITextField!
+    @IBOutlet var ratingTF: UITextField!
+    @IBOutlet var studioTF: UITextField!
+
+    var movie: Movie?
+
     override func viewDidLoad() {
         super.viewDidLoad()
+    }
+
+    @IBAction func updateButton(_ sender: UIButton) {
+        if let title = titleTF.text, let rating = ratingTF.text, let studio = studioTF.text, let m = movie {
+            update(movie_id: m.movie_id!, title: title, rating: rating, studio: studio)
+        }
+    }
+
+    func update(movie_id: Int, title: String, rating: String, studio: String) {
+        print("Updating movie with id: \(movie_id) \nTitle: \(title) \nRating: \(rating) \nStudio: \(studio)")
     }
 }
